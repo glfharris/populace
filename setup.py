3ddfed41d4eb4d16a9dd57feabd5844490ceaff4
George L. F. Harris <glfharris@gmail.com>
2017-01-05 16:52:46 +0000

    More fiddling

--- setup.py
+++ setup.py
@@ -1,14 +1,17 @@
-from disutils.core import setup
+from setuptools import setup
 
 setup(
     name = 'populace',
     packages = ['populace'],
     version = '0.0.1',
-    description = 'A python package for visualising population stats.'
+    description = 'A python package for visualising population stats.',
     author = 'George L. F. Harris',
     author_email = 'glfharris@gmail.com',
     url = 'https://github.com/glfharris/populace',
     download_url = 'https://github.com/glfharris/populace/tarball/0.0.1',
     keywords = ['visualization', 'population', 'graphic'],
     classifiers = [],
+    install_requires=[
+          'svgwrite',
+      ]
 )
